03b1e7b74acadb3542f4384ddb1fbf3861ef084f
tommybrits951 <tommybrits74@gmail.com>
2023-03-22 09:15:56 -0700

    i think i fixed it

--- api/auth/auth-middlware.js
+++ api/auth/auth-middlware.js
@@ -6,7 +6,7 @@ async function checkUsername(req, res, next) {
     try {
         const {username} = req.body;
         const users = await Users.getBy(username)
-        if (users.length < 1) {
+        if (!users) {
             next()
         } else {
             res.status(422).json({message: "username taken"})
