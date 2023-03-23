0130a9b1d7a9952c5150cf1796cc486537046ce1
tommybrits951 <tommybrits74@gmail.com>
2023-03-23 09:54:00 -0700

    h

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = require('./config/config')
 module.exports = (req, res, next) => {
-  const { token } = req.header.authorization;
+  const { token } = req.headers.authorization;
   if (!token) {
     res.status(422).json({message: "token required"})
   } else {
-    jwt.verify(req.header.authorization, JWT_SECRET, (err, decodedToken) => {
+    jwt.verify(req.headers.authorization, 'shh', (err, decodedToken) => {
       if (err) {
         res.status(401).json({message: err.message || 'token invalid'})
       } else {
