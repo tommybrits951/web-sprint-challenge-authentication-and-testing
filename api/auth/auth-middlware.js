7fb275781997291d30dbc92ddcdd46e4e9b4a43e
tommybrits951 <tommybrits74@gmail.com>
2023-03-23 06:06:23 -0700

    h

--- api/auth/auth-middlware.js
+++ api/auth/auth-middlware.js
@@ -20,11 +20,8 @@ async function checkCred(req, res, next) {
     try {
         const {username, password} = req.body
         const user = await Users.getBy(username)
-        if (!user) {
-            res.status(401).json({message: 'invalid credentials'})
-        } else if (!bcrypt.compareSync(password, user.password)) {
-            res.status(401).json({message: 'invalid credentials'})
-        } else if (!username || !password) {
+        
+        if (!username || !password) {
             
             res.status(401).json({message: 'username and password required'})
         } else {
